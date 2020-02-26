0f76c574c1b2b9ad43e8a04d39a03abf17181c6d
jasperfr <jasperfritse2@gmail.com>
2020-02-26 23:13:45 +0100

    styling and feature stuff

--- js/game_jp.js
+++ js/game_jp.js
@@ -5,7 +5,7 @@
  */
 
 // Convert an integer to a byte string.
-const prefix = ['bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB', 'KYB', 'MYB', 'GYB', 'TYB', 'PYB', 'EYB', 'ZYB', 'YYB'];
+const prefix = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB', 'KYB', 'MYB', 'GYB', 'TYB', 'PYB', 'EYB', 'ZYB', 'YYB'];
 const toByteCode = function(long) {
     let j = long;
     let str = `${j.toFixed(0)} ${prefix[0]}`;
@@ -47,97 +47,63 @@ $(document).ready(() => {
  *
  */
 generators = [];
-multiplicators = [];
 
 /* =======================================================================================================
  *
  * The HTML element used for generators.
  *
  */
-const HTML_MULTIPLICATOR = `
-<div class="multiplicator">
-    <div class="multicount">
-        <h1>[UNDEFINED]</h1>
-        <h3>+ ×1.001 / tick</h1>
-    </div>
-    <div class="upgrades">
-        <button class="tooltip buy-increase">
-            <span class="tooltiptext">Increase the multiplicator's multiplier by ×0.0001.</span>
-            <span class="label-increase">+ ×0.0001 / tick</span>
-        </button>
-    </div>
-</div>
-`;
-
 const HTML_ELEMENT = `
-    <fieldset class="generator">
-        <legend>Generator 1 - 1B/sec</legend>
-
-        <div class="upgrades">
-            <div class="upgrade">
-                <button class="tooltip buy-value">
-                    <span class="tooltiptext">Increases output value.</span>
-                    <span class="label-value">[UNDEFINED]</span>
-                </button>
-                <p class="cost cost-value">[UNDEFINED]</p>
-            </div>
-
-            <div class="upgrade">
-                <button class="tooltip buy-speed">
-                    <span class="tooltiptext">Increases output speed.</span>
-                    <span class="label-speed">[UNDEFINED]</span>
-                </button>
-                <p class="cost cost-speed">[UNDEFINED]</p>
-            </div>
-            
-            <div class="upgrade">
-                <button class="tooltip buy-ascend">
-                    <span class="tooltiptext">Lose all progress, but get a boost on future upgrades.<br><span class="red">This will cost 95% of your bytes.</span></span>
-                    <span class="label-ascend">[UNDEFINED]</span>
-                </button>
-                <p class="cost cost-ascend">[UNDEFINED]</p>
-            </div>
-        </div>
-
-        <div class="progress">
-            <div class="pbar slow">
-            </div>
-            <span class="text">0%</span>
-        </div>
-
-    </fieldset>
-    <br>
-`
+<div class="bit generator">
+    <table>
+            <tr>
+                <td colspan="2" class="output">
+                    1Bを生成します。
+                </td>
+            </tr>
+            <tr>
+                <th>
+                    <button class="tooltip buy-value">
+                        <span class="tooltiptext">Increases output value.</span>
+                        <span class="label-value">[UNDEFINED]</span>
+                    </button>
+                </th>
+                <th class="cost cost-value">[UNDEFINED]</th>
+            </tr>
+            <tr>
+                <th>
+                    <button class="tooltip buy-speed">
+                        <span class="tooltiptext">Increases output speed.</span>
+                        <span class="label-speed">[UNDEFINED]</span>
+                    </button>
+                </th>
+                <th class="cost cost-speed">[UNDEFINED]</th>
+            </tr>
+            <tr>
+                <th>
+                    <button class="tooltip buy-ascend">
+                        <span class="tooltiptext">Lose all progress, but get a boost on future upgrades.<br><span class="red">This will cost 95% of your bytes.</span></span>
+                        <span class="label-ascend">[UNDEFINED]</span>
+                    </button>
+                </th>
+                <th class="cost cost-ascend">[UNDEFINED]</th>
+            </tr>
+            <tr>
+                <th colspan="2" class="progress">
+                    <span class="text">0%</span>
+                    <span class="pbar"></span>
+                </th>
+            </tr>
+    </table>
+</div>`;
 
 /* =======================================================================================================
  *
  * The Generator class.
  *
  */
-class Multiplicator {
-    constructor(price) {
-        this.multiplier = 1.0;
-        this.multiIncrease = 0.0001;
-        this.element = $(HTML_MULTIPLICATOR);
-
-        $('#generators').append(this.element);
-        $((this.element).find('button')).click(() => this.buyMultiplierUpgrade());
-    }
-    tick() {
-        this.multiplier += this.multiIncrease;
-
-        $($(this.element).find('h1')).text(`×${this.multiplier.toFixed(3)}`); //×1.001 / tick
-        $($(this.element).find('h3')).text(`+ ×${this.multiIncrease.toFixed(4)} / tick`); //×1.001 / tick
-    }
-    buyMultiplierUpgrade() {
-        this.multiIncrease += 0.0001;
-    }
-}
-
-
 class Generator {
     constructor(price) {
-        this.genID = generators.length + 1;
         this.isMultiplier = false;
         this.active = false;
         this.price = price;
@@ -149,7 +115,7 @@ class Generator {
         this.upgrades = {
             value : { base : 1.0, baseprice : 2.0, costscale : 1.0, amount : 1.0, price : 2.0, purchased : 0, increment : 1.75 },
             speed : { base : 1.0, baseprice : 2.0, costscale : 1.0, amount : 1.0, price : 2.0, purchased : 0, increment: 1.5 },
-            ascend : { base : 1.0, baseprice : 1000.0, costscale : 1.0, amount : 1.0, price : 1000.0, purchased : 0, increment : 1.05 },
+            ascend : { base : 1.0, baseprice : 1000.0, costscale : 1.0, amount : 1.0, price : 1000.0, purchased : 0, increment : 1.25 },
         };
 
         // Create a button to unlock this generator.
@@ -175,6 +141,7 @@ class Generator {
         // Add a new generator, but:
         // - This generator is 1.000x this price.
         // - The generator count must not exceed 8. There is no such thing as a 9th generator.
+        if(generators.length < 8)
             generators.push(new Generator(this.price * 1_000));
 
         // Modify the HTML element.
@@ -187,12 +154,9 @@ class Generator {
         $($(this.element).find('.cost-speed')).text(toByteCode(this.upgrades.speed.price));
         $($(this.element).find('.cost-ascend')).text(toByteCode(this.upgrades.ascend.price));
 
-        $($(this.element).find('.label-value')).text(`Value ×${this.upgrades.value.increment}`);
-        $($(this.element).find('.label-speed')).text(`Speed ×${this.upgrades.speed.increment}`);
-        $($(this.element).find('.label-ascend')).text(`Ascend ×${this.upgrades.ascend.increment}`);
-
-        $(this.element).hide().slideDown();
-        this.setGeneratorLabel();
+        $($(this.element).find('.label-value')).text(`価値 x${this.upgrades.value.increment}`);
+        $($(this.element).find('.label-speed')).text(`速度 x${this.upgrades.speed.increment}`);
+        $($(this.element).find('.label-ascend')).text(`上昇 x${this.upgrades.ascend.increment}`);
     }
 
     // Buy a speed upgrade for this generator.
@@ -206,13 +170,6 @@ class Generator {
         this.upgrades.speed.price *= Math.pow(this.upgrades.speed.purchased * this.upgrades.speed.costscale, 1.5) + 2;
         // Set HTML
         $($(this.element).find('.cost-speed')).text(toByteCode(this.upgrades.speed.price));
-
-
-        let speed = this.upgrades.speed.amount * this.upgrades.ascend.amount;
-        if(speed >= 10) {
-            $($(this.element).find('.pbar')).removeClass('slow');
-        }
-        this.setGeneratorLabel();
     }
 
     // Buy a value upgrade for this generator.
@@ -224,17 +181,8 @@ class Generator {
         this.upgrades.value.amount *= this.upgrades.value.increment;
         this.upgrades.value.price *= 3 * this.upgrades.value.costscale;
         // Set HTML
-        $($(this.element).find('.output')).text(`Generate ${toByteCode(this.upgrades.value.amount)}.`);
+        $($(this.element).find('.output')).text(`${toByteCode(this.upgrades.value.amount)}を生成。`);
         $($(this.element).find('.cost-value')).text(toByteCode(this.upgrades.value.price));
-        this.setGeneratorLabel();
-    }
-
-    setGeneratorLabel() {
-        let speed = this.upgrades.speed.amount * this.upgrades.ascend.amount;
-        let multi = 1.0;
-        for(let m of multiplicators) { multi *= m.multiplier; }
-        let bps = 100.0 * this.upgrades.value.amount * this.upgrades.ascend.amount * multi * (speed / 100.0);
-        $($(this.element).find('legend')).text(`Generator ${this.genID} - ${toByteCode(bps)} / s`);
     }
 
     // Buy an ascension upgrade for this generator.
@@ -253,12 +201,12 @@ class Generator {
         this.upgrades.value.price = this.upgrades.value.baseprice;
         this.upgrades.value.increment *= this.upgrades.ascend.increment;
         this.upgrades.value.purchased = 0;
-        this.upgrades.value.costscale *= 1.03; // todo fix
+        this.upgrades.value.costscale *= 1.3; // todo fix
         this.upgrades.speed.amount = this.upgrades.speed.base;
         this.upgrades.speed.price = this.upgrades.speed.baseprice;
         this.upgrades.speed.increment *= this.upgrades.ascend.increment;
         this.upgrades.speed.purchased = 0;
-        this.upgrades.speed.costscale *= 1.03; // todo fix
+        this.upgrades.speed.costscale *= 1.3; // todo fix
         setValue(values.bytes * -0.95);
 
         // Modify the HTML element.
@@ -266,44 +214,27 @@ class Generator {
         $($(this.element).find('.cost-speed')).text(toByteCode(this.upgrades.speed.price));
         $($(this.element).find('.cost-ascend')).text(toByteCode(this.upgrades.ascend.price));
 
-        $($(this.element).find('.label-value')).text(`Value ×${this.upgrades.value.increment.toFixed(3)}`);
-        $($(this.element).find('.label-speed')).text(`Speed ×${this.upgrades.speed.increment.toFixed(3)}`);
-        $($(this.element).find('.label-ascend')).text(`Ascend ×${this.upgrades.ascend.increment.toFixed(3)}`);
-
-        this.setGeneratorLabel();
+        $($(this.element).find('.label-value')).text(`Value x${this.upgrades.value.increment}`);
+        $($(this.element).find('.label-speed')).text(`Speed x${this.upgrades.speed.increment}`);
+        $($(this.element).find('.label-ascend')).text(`Ascend x${this.upgrades.ascend.increment}`);
     }
 
     // A "tick" of the generator.
     tick() {
         if(!this.active) return;
-
-        let speed = this.upgrades.speed.amount * this.upgrades.ascend.amount;
-        let multi = 1.0;
-        for(let m of multiplicators) { multi *= m.multiplier; }
-
-        if(speed < 10) {
-            this.progress += this.upgrades.speed.amount * this.upgrades.ascend.amount;
-            if(this.progress >= 100) {
-                this.progress = 0;
-                setValue(this.upgrades.value.amount * this.upgrades.ascend.amount * multi);
-            }
-            $($(this.elements.progressbar).find('.text')).text(`${this.progress.toFixed(2)}%`);
-            $($(this.elements.progressbar).find('.pbar')).css('width', `${this.progress}%`);
+        this.progress += this.upgrades.speed.amount * this.upgrades.ascend.amount;
+        if(this.progress >= 100) {
+            this.progress = 0;
+            setValue(this.upgrades.value.amount * this.upgrades.ascend.amount);
         }
-
-        else {
-            setValue(this.upgrades.value.amount * this.upgrades.ascend.amount * multi * (speed / 100.0));
-            $($(this.elements.progressbar).find('.text')).text(`${(speed / 100.0).toFixed(2)}x / tick`);
-            $($(this.elements.progressbar).find('.pbar')).css('width', `100%`);
-        }
+        $($(this.elements.progressbar).find('.text')).text(`${this.progress.toFixed(2)}%`);
+        $($(this.elements.progressbar).find('.pbar')).css('width', `${this.progress}%`);
+        // TODO : Fix progress bar when it goes to insane speed.*/
     }
 }
 
 // Add an interval for each game tick.
 setInterval(() => {
-    for(let m of multiplicators) {
-        m.tick();
-    }
     for(let g of generators) {
         g.tick();
     }
