a8de7ec321ab6f6324a8dc964e0bd6c5bb213f75
Jasper Fritse <jasper.fritse@windesheim.nl>
2022-01-21 01:54:34 +0100

    threecraft acting weird, removign for now

--- apps/threecraft/src/render/floor.js
+++
@@ -1,7 +0,0 @@
-const renderFloor = function(scene) {
-    scene.add(ThreeUtils.createWall('bedrock', 152, -8, 152, 320, 320, (-Math.PI / 2), 0, 0, true));
-    scene.add(ThreeUtils.createWall('bedrock', 152, 32, -8, 320, 80, 0, 0, 0, true));
-    scene.add(ThreeUtils.createWall('bedrock', 152, 32, -8+320, 320, 80, 0, 0, 0, true));
-    scene.add(ThreeUtils.createWall('bedrock', -8+320, 32, 152, 320, 80, 0, (-Math.PI/2), 0, true));
-    scene.add(ThreeUtils.createWall('bedrock', -8, 32, 152, 320, 80, 0, (-Math.PI/2), 0, true));
-};
